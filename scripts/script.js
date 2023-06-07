c837abc81caba590b16cf9c420b25e2913e50c8a
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-06-07 03:09:45 -0300

    Aumentando a letra do site

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,1 @@
 
-alt="Gráfico sobre fome"
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
-
-var myCarousel = document.getElementById('carouselExampleDark')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000, // Tempo em milissegundos entre cada slide
-  wrap: true // O carrossel continua a rodar indefinidamente
-})

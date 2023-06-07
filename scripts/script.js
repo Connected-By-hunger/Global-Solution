b3f1367df1637e078ea76b4f38504920d6f3875a
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-06-07 04:01:15 -0300

    Adicionando informações da fome mundial

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 
+var myCarousel = document.getElementById('carouselExampleDark2')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000, // Tempo em milissegundos entre cada slide
+  wrap: true // O carrossel continua a rodar indefinidamente
+})
+
+
+
+
